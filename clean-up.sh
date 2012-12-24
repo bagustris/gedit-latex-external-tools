2389cbb019bfbf744ef734e83c8904da147d16b6
rjdsc <github@rjdsc.org>
2012-12-24 14:05:54 -0300

    Fixed rm loop

--- clean-up.sh
+++ clean-up.sh
@@ -14,11 +14,28 @@ shortname=${filename%.*}
 pathname=$GEDIT_CURRENT_DOCUMENT_PATH
 directory=`dirname "$pathname"`
 
-#echo $filename
-#echo $shortname
-#echo $pathname
-#echo $directory
+# Standard garbage
+extensions="acn acr alg aux bbl blg dvi \
+            fdb_latexmk glg glo gls idx \
+            ilg ind ist lof lol log lot \
+            lyx maf mp mtc mtc1 nav nlo \
+            out pdfsync snm spl synctex \
+            synctex.gz tmp toc top tui vrb \
+            bak tex.backup \
+            swp"
 
-rm -f "$directory"/"$shortname.ps" "$directory"/*.aux "$directory"/*.log "$directory"/*.toc "$directory"/*.bbl "$directory"/*.blg "$directory"/*.idx "$directory"/*.out "$directory"/*.ind "$directory"/*.ilg "$directory"/*.bak "$directory"/*.synctex "$directory"/*.nav "$directory"/*.snm "$directory"/*.lol "$directory"/*.lof "$directory"/*.spl "$directory"/*.lyx "$directory"/*.synctex "$directory"/*.lot "$directory"/*~ "$directory"/*.pdfsync "$directory"/*.synctex.gz "$directory"/"#"*"#" "$directory"/.??*xml "$directory"/*.tex.backup
+for databaseName in $extensions; do
+  rm -f "$directory"/*.$databaseName
+done 
 
+# Remove PS output? All we want is the final output, right?
+rm -f "$directory"/"$shortname.ps"
+
+# More garbage
+rm -f "$directory"/*~ \
+      "$directory"/"#"*"#" \
+      "$directory"/.??*xml
+
+# Some LaTeX editors create gargae directories
 rm -fR "$directory"/auto
+
