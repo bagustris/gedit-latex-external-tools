adf7331ed2dde70145efa81cd9937ab67acd30d4
rjdsc <git@rjdsc.org>
2015-02-02 21:14:56 +0100

    Included bundledoc tool

---
+++ bundle-doc.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# [Gedit Tool]
+# Name=Bundledoc
+# Shortcut=<Primary>8
+# Languages=latex
+# Applicability=titled
+# Output=nothing
+# Input=nothing
+# Save-files=nothing
+
+filename=$GEDIT_CURRENT_DOCUMENT_NAME
+basename=${filename%.*}
+
+CONFIG_FILE=`eval echo  "~/.config/gedit/tools/bd-zip.cfg"`
+
+bundledoc --localonly \
+          --config=$CONFIG_FILE \
+          --listdeps=yes \
+          --nokeepdirs \
+          --exclude=.out \
+          --manifest="" \
+          "$basename".dep
+
